b725fbb4c392a1f7e4fe84feb39cd99b1ec320d6
Quyet8610 <vipcold.quyet@gmail.com>
2023-01-03 10:25:12 +0700

    First_commit_BTQ499

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiTrungQuyet499.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
